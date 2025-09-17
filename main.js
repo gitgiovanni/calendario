a34e7eba25191cf3103cbb0267ccbfe02997b255
Giovanni Oséas <111138894+gitgiovanni@users.noreply.github.com>
2025-09-17 20:11:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -113,6 +113,18 @@ function load (){
   }
 }
 
+function exportToPdf() {
+  const element = document.getElementById('container');
+  const opt = {
+    margin:       1,
+    filename:     'calendario.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+  };
+  html2pdf().set(opt).from(element).save();
+}
+
 function closeModal(){
   eventTitleInput.classList.remove('error')
   newEvent.style.display = 'none'

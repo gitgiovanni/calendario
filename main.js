72d02518ebe18306a65b96cf1199fc7bbf7fedd6
Giovanni Oséas <111138894+gitgiovanni@users.noreply.github.com>
2025-09-17 20:16:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ function exportToPdf() {
     margin:       1,
     filename:     'calendario.pdf',
     image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 1.5 }, // Altere o valor da escala aqui
+    html2canvas:  { scale: 1 }, // Altere o valor da escala aqui
     jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
   };
   html2pdf().set(opt).from(element).save();
